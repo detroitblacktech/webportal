d47d514a0376686ebb3797dac9f03825710eeda5
Mack Hendricks <mack@dopensource.com>
2019-11-03 13:32:52 -0500

    Added a sleep 20 to allow the repo's to be updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,6 +30,7 @@ resource "digitalocean_droplet" "webserver" {
           "sudo apt-get update",
           "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common git",
           "curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -",
+	  "sleep 20",
           "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian$(lsb_release -cs) stable\"",
           "sudo apt-get update",
           "sudo apt-get install docker-ce docker-ce-cli containerd.io",
