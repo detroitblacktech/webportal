2a7cb182ce670790fe5cb046642bf11916a11bbe
Mack Hendricks <mack@dopensource.com>
2019-12-21 08:21:43 -0500

    Fixed error in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "digitalocean_droplet" "webserver" {
           "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common git",
           "curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -",
 	  "sleep 20",
-          "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debiani $(lsb_release -cs) stable\"",
+          "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable\"",
           "sudo apt-get update",
           "sleep 20",
           "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
