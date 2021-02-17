33aca343b4793611bb7cdaf9b3e3701a8c1cccdf
Mack Hendricks <mack@dopensource.com>
2021-02-16 20:27:07 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Check if in development mode
-apt-get updates && apt-get install -y nginx && cp ~/webportal/build/nginx.conf /etc/nginx/sites-available/default && systemctl restart nginx
+apt-get update && apt-get install -y nginx && cp ~/webportal/build/nginx.conf /etc/nginx/sites-available/default && systemctl restart nginx
 
 if [ "$2" == "dev" ]; then
 	volumemount="-v $(pwd):/usr/src/dbt"
